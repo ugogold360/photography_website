7875a1b6a99bd5aa8749a56f6495283a0afcbb43
Obed Ugochukwu <emperorgold360@gmail.com>
2020-04-04 01:00:47 +0100

    added mlab link to DB

--- config/DB/productionDB.js
+++ config/DB/productionDB.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoDBUrl: 'the url to the mlab database goes here' //Note that in real production, this url should be addded in heroku env and then used here (process.env.MONGO_DB_URL, where mongo db url is the env created in heroku)
+    mongoDBUrl: 'mongodb://photography_website:photography_website149@ds117858.mlab.com:17858/photography_website' //Note that in real production, this url should be addded in heroku env and then used here (process.env.MONGO_DB_URL, where mongo db url is the env created in heroku)
 }
